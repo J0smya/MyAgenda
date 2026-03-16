2df505c0c9a3cb245ee52259c76ab0ad959a9d55
yardelG29 <yardelfelipe.g@gmail.com>
2026-03-16 04:31:09 -0500

    Se agrega funcionalidad de etiquetas multiples y conexion con base de datos

--- proyecto/api/src/routes/todos.routes.js
+++ proyecto/api/src/routes/todos.routes.js
@@ -1,8 +1,31 @@
 import { Router } from "express";
 import prisma from "../db/prisma.js";
+
 const router = Router();
+
 router.get("/", async (req, res) => {
  const todos = await prisma.todo.findMany();
  res.json(todos);
 });
+
+router.post("/", async (req, res) => {
+ try {
+
+  const { titulo, descripcion, etiquetas } = req.body;
+
+  const nuevaTarea = await prisma.todo.create({
+   data: {
+    titulo,
+    descripcion,
+    etiquetas
+   }
+  });
+
+  res.json(nuevaTarea);
+
+ } catch (error) {
+  res.status(500).json({ error: error.message });
+ }
+});
+
 export default router;
