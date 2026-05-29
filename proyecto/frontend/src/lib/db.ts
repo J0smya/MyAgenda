0e15506c04df47fa5eece76df6e7ab6d63836ae2
german663 <germanandrescalderonsilvestre@gmail.com>
2026-05-29 14:18:40 -0500

    bd.ts modificado para el despliegue

--- proyecto/frontend/src/lib/db.ts
+++ proyecto/frontend/src/lib/db.ts
@@ -1,14 +1,20 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// Esta variable tomará el valor de DATABASE_URL desde Render
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Requerido para Neon
+    rejectUnauthorized: false,
   },
 });
 
 pool.connect()
-  .then(() => console.log("Conectado a Neon PostgreSQL ✅"))
+  .then(() => console.log("Conectado a PostgreSQL ✅"))
   .catch(err => console.error("Error conexión ❌", err));
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
